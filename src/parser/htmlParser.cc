675d4fee176aaa55c903d7c9b8f775dc38c59b6b
Rakin Rahman <rakinhacker@gmail.com>
2022-10-07 19:33:33 +0600

    Add implementation

--- src/parser/htmlParser.cc
+++ src/parser/htmlParser.cc
@@ -1,1 +1,13 @@
 #include "htmlParser.h"
+
+#include <fstream>
+#include <string_view>
+
+namespace hp
+{
+    HtmlParser::HtmlParser(std::string_view str) {}
+
+    HtmlParser::HtmlParser(std::fstream file) {}
+
+    std::string_view HtmlParser::decode() {}
+} // namespace hp
