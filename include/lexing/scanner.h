1960d7fc1faa04d282a143a7ea69a265e2a2358c
Rakin Rahman <rakinhacker@gmail.com>
2022-10-08 13:44:49 +0600

    Finish writing scanner class

--- include/lexing/scanner.h
+++ include/lexing/scanner.h
@@ -1,8 +1,28 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+#include "token.h"
+
+#include <string_view>
+#include <vector>
+
 namespace hp::lexing
 {
+    class Scanner
+    {
+    public:
+        explicit Scanner(std::string_view source);
+        std::vector<Token> scanTokens();
+
+    private:
+        std::string_view m_source {};
+        std::vector<Token> m_tokens {};
+        int m_start { 0 };
+        int m_current { 0 };
+        int m_line { 1 };
+
+        bool isAtEnd();
+    };
 } // namespace hp::lexing
 
 #endif
--- src/parser/lexing/scanner.cc
+++ src/parser/lexing/scanner.cc
@@ -1,1 +1,30 @@
 #include "lexing/scanner.h"
+
+#include "lexing/token.h"
+#include "lexing/tokenType.h"
+
+#include <string_view>
+#include <vector>
+
+namespace hp::lexing
+{
+    Scanner::Scanner(std::string_view source) : m_source(source) {}
+
+    std::vector<Token> Scanner::scanTokens()
+    {
+        while (!isAtEnd())
+        {
+            // We are at the beginning of the next lexeme
+            m_start = m_current;
+            scanTokens();
+        }
+
+        m_tokens.emplace_back(TokenType::EOF, "", 0, m_line);
+        return m_tokens;
+    }
+
+    bool Scanner::isAtEnd()
+    {
+        return static_cast<std::size_t>(m_current) >= m_source.size();
+    }
+} // namespace hp::lexing
