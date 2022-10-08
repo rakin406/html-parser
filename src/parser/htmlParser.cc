1b891dd89ee0be0654591cc6b190d9c84d1a21da
Rakin Rahman <rakinhacker@gmail.com>
2022-10-08 19:04:06 +0600

    Remove namespace function

--- src/parser/htmlParser.cc
+++ src/parser/htmlParser.cc
@@ -7,31 +7,6 @@
 #include <string_view>
 #include <vector>
 
-namespace
-{
-    // TODO: Add indent after newline
-    /**
-     * @brief Add newline after closing tags.
-     *
-     * @param document HTML document.
-     */
-    void addNewlineAfterClosingTags(std::string& document)
-    {
-        std::size_t length { document.size() };
-        // std::string_view lastTag {};
-        for (std::size_t i = 0; i < length; ++i)
-        {
-            // Match closing tag character '>' and ensure that the letter after
-            // '>' is not a newline.
-            if ((document[i] == '>') && (++i < length) &&
-                (document[++i] != '\n'))
-            {
-                document.insert(++i, "\n");
-            }
-        }
-    }
-} // namespace
-
 namespace hp
 {
     HtmlParser::HtmlParser(std::string_view str) : m_document(str) {}
