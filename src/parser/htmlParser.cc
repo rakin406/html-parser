ad6ee3e05a8348f7b4c52a7ce166dc53195134f6
Rakin Rahman <rakinhacker@gmail.com>
2022-10-08 12:55:15 +0600

    Add token.h and token.cc

--- src/parser/htmlParser.cc
+++ src/parser/htmlParser.cc
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace
 {
@@ -53,4 +54,22 @@ namespace hp
     }
 
     // std::string_view HtmlParser::decode() {}
+
+    // void HtmlParser::PageElement::append(std::string_view tag) {}
+    // void HtmlParser::PageElement::extract() {}
+
+    // void HtmlParser::Tag::decompose() {}
+    //
+    // std::string_view HtmlParser::Tag::findChild() {}
+    //
+    // std::vector<std::string_view> HtmlParser::Tag::findChildren() {}
+    //
+    // std::string_view HtmlParser::Tag::get() {}
+    //
+    // std::vector<std::string_view> HtmlParser::Tag::getText() {}
+    //
+    // bool HtmlParser::Tag::hasKey() {}
+    //
+    // bool HtmlParser::Tag::isEmptyElement() {}
+
 } // namespace hp
